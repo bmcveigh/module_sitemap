9c92d1129ed1330938f2766f348f6b88721b6b4f
Brian McVeigh <brianmcveigh@Brians-MBP.fios-router.home>
2016-09-24 17:58:27 -0400

    Fixed some security issues.

--- src/Form/AdminSettingsForm.php
+++ src/Form/AdminSettingsForm.php
@@ -9,6 +9,7 @@ namespace Drupal\module_sitemap\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Html;
 
 /**
  * TODO: class docs.
@@ -56,8 +57,8 @@ class AdminSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('module_sitemap.settings')
-      ->set('show_links_with_no_title', $form_state->getValue('show_links_with_no_title'))
-      ->set('display_full_url', $form_state->getValue('display_full_url'))
+      ->set('show_links_with_no_title', Html::escape($form_state->getValue('show_links_with_no_title')))
+      ->set('display_full_url', Html::escape($form_state->getValue('display_full_url')))
       ->save();
 
     parent::submitForm($form, $form_state);
